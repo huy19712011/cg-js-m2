9b917a0a99cd0e9dc3f202808cb3bf65f2c2d8ca
huynq <huy19712011@gmail.com>
2022-11-22 11:09:46 +0700

    part 13: sorting

---
+++ part13-sortAlgorithm/sort.ts
@@ -0,0 +1,20 @@
+// 1. Overview
+// sort ~ find, insert
+
+//Since they can often reduce the complexity of a problem, sorting algorithms are very important in computer science.
+//These algorithms have direct applications in searching algorithms, database algorithms, divide and conquer methods,
+// data structure algorithms, and many more.
+
+
+// some main algorithms
+// Bubble sort
+// Selection sort
+// Insertion sort
+// Merge sort
+// Quick sort
+// Heap sort
+// Counting sort
+// Radix sort
+// Bucket sort
+
+
